b5d5399e8e0134b9103a3cc9e8e9f3b7d532558c
Lee Tae Yong <bpthess@naver.com>
2022-04-29 01:27:50 +0900

    커밋

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,8 @@ $(function () {
     /* Direct */
     // Scroll Mode
     var s = skrollr.init({
-        smoothScrolling: true
+        smoothScrolling: true,
+        smoothScrollingDuration: 500
     });
 
     window.addEventListener("scroll", () => {
@@ -15,43 +16,48 @@ $(function () {
     })
 
 
+
+
+
+
+
     // Skill Animation
-    function drawCircle(container, id, progress, parent) {
-        var paper = Snap(container);
-        var prog = paper.path("M5,50 A45,45,0 1 1 95,50 A45,45,0 1 1 5,50");
-        var lineL = prog.getTotalLength();
-        var oneUnit = lineL / 100;
-        var toOffset = lineL - oneUnit * progress;
-        var myID = 'circle-graph-' + id;
-        prog.attr({
-            'stroke-dashoffset': lineL,
-            'stroke-dasharray': lineL,
-            'id': myID
-        });
-
-        var animTime = 1300/*progress / 100*/
-
-        prog.animate({
-            'stroke-dashoffset': toOffset
-        }, animTime, mina.easein);
-
-        function countCircle(animtime, parent, progress) {
-            var i = 0;
-            var time = 1300;
-            var intervalTime = Math.abs(time / progress);
-            var timerID = setInterval(function () {
-                i++;
-                textContainer.text(i + "%");
-                if (i === progress) clearInterval(timerID);
-            }, intervalTime);
-        }
-
-        countCircle(animTime, parent, progress);
-    }
-
-    $(window).on('load', function () {
-        drawCircle('.chart-circle', 1, 77, '.circle');
-    });
+    //    function drawCircle(container, id, progress, parent) {
+    //        var paper = Snap(container);
+    //        var prog = paper.path("M5,50 A45,45,0 1 1 95,50 A45,45,0 1 1 5,50");
+    //        var lineL = prog.getTotalLength();
+    //        var oneUnit = lineL / 100;
+    //        var toOffset = lineL - oneUnit * progress;
+    //        var myID = 'circle-graph-' + id;
+    //        prog.attr({
+    //            'stroke-dashoffset': lineL,
+    //            'stroke-dasharray': lineL,
+    //            'id': myID
+    //        });
+    //
+    //        var animTime = 1300/*progress / 100*/
+    //
+    //        prog.animate({
+    //            'stroke-dashoffset': toOffset
+    //        }, animTime, mina.easein);
+    //
+    //        function countCircle(animtime, parent, progress) {
+    //            var i = 0;
+    //            var time = 1300;
+    //            var intervalTime = Math.abs(time / progress);
+    //            var timerID = setInterval(function () {
+    //                i++;
+    //                textContainer.text(i + "%");
+    //                if (i === progress) clearInterval(timerID);
+    //            }, intervalTime);
+    //        }
+    //
+    //        countCircle(animTime, parent, progress);
+    //    }
+    //
+    //    $(window).on('load', function () {
+    //        drawCircle('.chart-circle', 1, 77, '.circle');
+    //    });
 
 
 
@@ -90,52 +96,48 @@ $(function () {
 
 
     var gauge1 = Gauge(
-        document.getElementById("gauge1"),
-        {
+        document.getElementById("gauge1"), {
             max: 100,
             dialStartAngle: -90,
             dialEndAngle: -90.001,
             value: 100,
-            label: function(value) {
+            label: function (value) {
                 return Math.round(value) + "/" + this.max;
             }
         }
     )
     gauge1();
     var gauge2 = Gauge(
-        document.getElementById("gauge2"),
-        {
+        document.getElementById("gauge2"), {
             max: 100,
             dialStartAngle: -90,
             dialEndAngle: -90.001,
             value: 100,
-            label: function(value) {
+            label: function (value) {
                 return Math.round(value) + "/" + this.max;
             }
         }
     )
     gauge2();
     var gauge3 = Gauge(
-        document.getElementById("gauge3"),
-        {
+        document.getElementById("gauge3"), {
             max: 100,
             dialStartAngle: -90,
             dialEndAngle: -90.001,
             value: 100,
-            label: function(value) {
+            label: function (value) {
                 return Math.round(value) + "/" + this.max;
             }
         }
     )
     gauge3();
     var gauge4 = Gauge(
-        document.getElementById("gauge4"),
-        {
+        document.getElementById("gauge4"), {
             max: 100,
             dialStartAngle: -90,
             dialEndAngle: -90.001,
             value: 100,
-            label: function(value) {
+            label: function (value) {
                 return Math.round(value) + "/" + this.max;
             }
         }
